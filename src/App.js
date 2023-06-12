8ed5497fe708fe127282cd6f240278064428bf43
Milan Chauhan <milanchauhan.development@gmail.com>
2023-06-13 02:40:00 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the hypertext version of myself</h1>
+      <div class="goku">
+        <img src="goku-02.jpeg" alt="" />
+        <h1>Dare to dream. Dare to build.</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
