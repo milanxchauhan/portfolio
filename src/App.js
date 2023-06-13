1bdd24c0a8e4274cac8d5a9705ff6b132fc43cbf
Milan Chauhan <milanchauhan.development@gmail.com>
2023-06-13 11:11:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the hypertext version of myself</h1>
+      <div className="Intro">
+      <p>I am a Computer Science undergrad at VIT Bhopal university and currently learning web development, product design and core computer science fundamentals.</p>
+      <p>If you're upto something cool, I'd love to hear about it. Even if you're not and just want to hang out and chill, i'm pretty much active on <a href="https://twitter.com/MilanxChauhan">twitter</a>.</p>
+      </div>
       <div class="goku">
-        <img src="goku-02.jpeg" alt="" />
+        <img src="goku-img.jpeg" alt="" />
         <h1>Dare to dream. Dare to build.</h1>
       </div>
     </div>
